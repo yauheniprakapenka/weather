fefbb869e56e89c6a00c6dd8e62ea3ce3f44b435
Yauheni Prakapenka <eprakopenko@intervale.ru>
2019-08-31 16:18:02 +0300

    Refactored MVC

--- jsonparse/Model/KindOfWeather.swift
+++ jsonparse/Model/KindOfWeather.swift
@@ -12,10 +12,10 @@ class KindOfWeather {
     
     let weather = Weather()
     
-    public let sunny = ["Sunny"]
-    public let cloudy = ["Freezing fog", "Partly cloudy", "Cloudy", "Overcast", "Mist", "Blizzard", "Fog"]
-    public let rainy = ["Patchy rain nearby", "Thundery outbreaks in nearby", "Patchy light drizzle", "Light drizzle", "Light rain", "Moderate rain at times", "Moderate rain", "Heavy rain at times", "Heavy rain", "Light freezing rain", "Light rain shower", "Moderate or heavy rain shower", "Torrential rain shower", "Light sleet showers", "Moderate or heavy sleet showers", "Patchy light rain in area with thunder", "Moderate or heavy rain in area with thunder"]
-    public let snow = ["Patchy snow nearby", "Patchy sleet nearby", "Patchy freezing drizzle nearby", "Blowing snow", "Freezing drizzle", "Moderate or heavy freezing rain", "Light sleet", "Moderate or heavy sleet", "Patchy light snow", "Light snow", "Patchy moderate snow", "Moderate snow", "Patchy heavy snow", "Heavy snow", "Ice pellets", "Light snow showers", "Moderate or heavy snow showers", "Light showers of ice pellets", "Moderate or heavy showers of ice pellets", "Patchy light snow in area with thunder", "Moderate or heavy snow in area with thunder"]
+    let sunny = ["Sunny"]
+    let cloudy = ["Freezing fog", "Partly cloudy", "Cloudy", "Overcast", "Mist", "Blizzard", "Fog"]
+    let rainy = ["Patchy rain nearby", "Thundery outbreaks in nearby", "Patchy light drizzle", "Light drizzle", "Light rain", "Moderate rain at times", "Moderate rain", "Heavy rain at times", "Heavy rain", "Light freezing rain", "Light rain shower", "Moderate or heavy rain shower", "Torrential rain shower", "Light sleet showers", "Moderate or heavy sleet showers", "Patchy light rain in area with thunder", "Moderate or heavy rain in area with thunder"]
+    let snow = ["Patchy snow nearby", "Patchy sleet nearby", "Patchy freezing drizzle nearby", "Blowing snow", "Freezing drizzle", "Moderate or heavy freezing rain", "Light sleet", "Moderate or heavy sleet", "Patchy light snow", "Light snow", "Patchy moderate snow", "Moderate snow", "Patchy heavy snow", "Heavy snow", "Ice pellets", "Light snow showers", "Moderate or heavy snow showers", "Light showers of ice pellets", "Moderate or heavy showers of ice pellets", "Patchy light snow in area with thunder", "Moderate or heavy snow in area with thunder"]
     
     func getKindOfWeather(kind: String) -> String {
         var gif = ""
