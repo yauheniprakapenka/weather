bd8ce4121a4b05df85bfc96bd0b7af8bc416fbb3
Yauheni Prakapenka <eprakopenko@intervale.ru>
2019-08-31 13:11:53 +0300

    Weather display added

--- jsonparse/weatherViewController.swift
+++ jsonparse/weatherViewController.swift
@@ -11,13 +11,12 @@ import UIKit
 class weatherViewController: UIViewController, UITextFieldDelegate {
     
     let weather = Weather()
-    let conditionWeatherJSON = ConditionWeatherJSON()
+    let kindOfWeather = KindOfWeather()
     
     @IBOutlet weak var weatherImageView: UIImageView!
     @IBOutlet weak var countryLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var conditionTextLabel: UILabel!
-    @IBOutlet weak var conditionIcon: UIImageView!
     @IBOutlet weak var inputCityFextField: UITextField!
 
     override func viewDidLoad() {
@@ -26,28 +25,38 @@ class weatherViewController: UIViewController, UITextFieldDelegate {
         inputCityFextField.layer.borderColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         inputCityFextField.alpha = 0.5
         inputCityFextField.layer.borderWidth = 1
-        inputCityFextField.layer.cornerRadius = 15
+        inputCityFextField.layer.cornerRadius = 10
         inputCityFextField.attributedPlaceholder = NSAttributedString(string: "Введите город", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
-        
-        weatherImageView.loadGif(name: "Sunny")
         
         self.inputCityFextField.delegate = self
         
-        conditionWeatherJSON.getCondition()
+        weather.getWeather(city: "grodno", completion: {
+            DispatchQueue.main.async {
+                self.setValue()
+            }
+        })
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         let inputCity = inputCityFextField.text!
         weather.getWeather(city: inputCity, completion: {
             DispatchQueue.main.async {
-                self.countryLabel.text = self.weather.country
-                self.temperatureLabel.text = self.weather.temperature
-                self.conditionTextLabel.text = self.weather.condititon
+                self.setValue()
             }
         })
         
         self.view.endEditing(true)
         return false
+    }
+    
+    func setValue() {
+        self.countryLabel.text = self.weather.country
+        self.temperatureLabel.text = self.weather.temperature
+        self.conditionTextLabel.text = self.weather.condititon
+        
+        // Выбрать GIF
+        let gif = self.kindOfWeather.getKindOfWeather(kind: self.weather.condititon)
+        self.weatherImageView.loadGif(name: gif)
     }
 
 }
